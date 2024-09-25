2887dfeb962ba8542993a4cf9dacc7610947584a
tarek7salamaa <tarek7salamaa@gmail.com>
2024-09-25 13:01:22 +0300

    Stack DS

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include "classes/linkedList.h"
+#include "classes/stack.h"
 using namespace std;
 
 int main()
 {
-    linkedList list;
-    list.insertAtEnd(1);
-    list.insertAtEnd(2);
-    list.insertAtEnd(3);
-
-    list.display();
     return 0;
 }
